612ff886447cd8faf277f09cddc58d06835af811
shunsuke-tamura <wwwxx4869@gmail.com>
2023-03-13 15:25:45 +0900

    update: 一旦切り取りができた

--- issue4-color-recognition/src/main.rs
+++ issue4-color-recognition/src/main.rs
@@ -2,8 +2,26 @@ fn main() {
     let mut lt = leptess::LepTess::new(None, "eng").unwrap();
     lt.set_image("test.png").unwrap();
     println!("{}", lt.get_utf8_text().unwrap());
-    // println!("hello!");
-    // for n in 1..11 {
-    //     println!("{n}Hello, world!");
-    // }
+    let boxes = lt
+        .get_component_boxes(leptess::capi::TessPageIteratorLevel_RIL_WORD, true)
+        .unwrap();
+
+    let mut img = image::open("test.png").unwrap();
+    for (i, b) in boxes.into_iter().enumerate() {
+        println!("{:?}", b);
+        let abox = b.get_geometry();
+        let _image = image::imageops::crop(
+            &mut img,
+            abox.x.try_into().unwrap(),
+            abox.y.try_into().unwrap(),
+            abox.w.try_into().unwrap(),
+            abox.h.try_into().unwrap(),
+        )
+        .to_image();
+
+        let mut img_name = "test".to_string();
+        img_name += &i.to_string();
+        img_name += ".png";
+        _image.save(img_name);
+    }
 }
